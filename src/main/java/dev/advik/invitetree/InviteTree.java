af739b3fc43a9c84bce6433059c1e10f388723c7
Advik <advik.b@gmail.com>
2024-06-25 22:02:26 +0530

    Update InviteTree.java

--- src/main/java/dev/advik/invitetree/InviteTree.java
+++ src/main/java/dev/advik/invitetree/InviteTree.java
@@ -1,6 +1,9 @@
 package dev.advik.invitetree;
 
+import dev.advik.invitetree.commands.Salt;
+import dev.advik.invitetree.datastore.StorageManager;
 import dev.advik.invitetree.listeners.LoginListener;
+import dev.advik.invitetree.commands.InviteDebug;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
 import java.util.logging.Logger;
@@ -8,18 +11,26 @@ import java.util.logging.Logger;
 public final class InviteTree extends JavaPlugin {
 
     private Logger logger;
+    private final String url = "jdbc:sqlite:players.db";
+    private StorageManager storageManager;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         logger = getLogger();
+        storageManager = new StorageManager(url, logger);
+        storageManager.connect();
+        storageManager.createTables();
         PluginManager pluginMGR = getServer().getPluginManager();
-        pluginMGR.registerEvents(new LoginListener(), this);
+        pluginMGR.registerEvents(new LoginListener(storageManager), this);
+        getCommand("inviteDebug").setExecutor(new InviteDebug(storageManager));
+        getCommand("salt").setExecutor(new Salt());
         logger.info("InviteTree has been enabled!");
     }
 
     @Override
     public void onDisable() {
+        storageManager.disconnect();
         // Plugin shutdown logic
         logger.info("InviteTree has been disabled!");
     }
