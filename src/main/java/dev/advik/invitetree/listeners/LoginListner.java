0784a34967cc4d41f62f85ba6511e972430ef2c9
Advik <advik.b@gmail.com>
2024-06-25 18:48:10 +0530

    Create LoginListner.java

---
+++ src/main/java/dev/advik/invitetree/listeners/LoginListner.java
@@ -0,0 +1,17 @@
+package dev.advik.invitetree.listeners;
+
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class LoginListner implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage("Welcome to the server!");
+    }
+
+}
