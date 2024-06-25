aa358fdbb3c306c40d55646299447324fefa96c2
Advik <advik.b@gmail.com>
2024-06-25 18:51:37 +0530

    Update InviteTree.java

--- src/main/java/dev/advik/invitetree/InviteTree.java
+++ src/main/java/dev/advik/invitetree/InviteTree.java
@@ -1,6 +1,6 @@
 package dev.advik.invitetree;
 
-import dev.advik.invitetree.listeners.LoginListner;
+import dev.advik.invitetree.listeners.LoginListener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
 import java.util.logging.Logger;
@@ -8,21 +8,19 @@ import java.util.logging.Logger;
 public final class InviteTree extends JavaPlugin {
 
     private Logger logger;
-    private PluginManager pluginMGR;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         logger = getLogger();
-        pluginMGR = getServer().getPluginManager();
-        pluginMGR.registerEvents(new LoginListner(), this);
+        PluginManager pluginMGR = getServer().getPluginManager();
+        pluginMGR.registerEvents(new LoginListener(), this);
         logger.info("InviteTree has been enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        // Unregister the listener
         logger.info("InviteTree has been disabled!");
     }
 }
