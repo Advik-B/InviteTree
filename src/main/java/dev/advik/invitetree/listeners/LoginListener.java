76996e80d89f3a1ebb7d55879b83a38c08006402
Advik <advik.b@gmail.com>
2024-06-25 19:44:08 +0530

    Update LoginListener.java

--- src/main/java/dev/advik/invitetree/listeners/LoginListener.java
+++ src/main/java/dev/advik/invitetree/listeners/LoginListener.java
@@ -1,19 +1,46 @@
 package dev.advik.invitetree.listeners;
 
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.format.TextColor.color;
 
 public class LoginListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        player.setGliding(true);
-        player.sendMessage("Welcome to the server!");
-        player.sendMessage("Fly around and have fun!");
+        TextComponent textComponent = text()
+                .content("You're a ")
+                .color(color(0x8FFAD))
+                .append(text().content("Bunny").color(NamedTextColor.LIGHT_PURPLE))
+                .append(text("! Press "))
+                .append(
+                        Component.keybind()
+                                .keybind("key.jump")
+                                .color(NamedTextColor.LIGHT_PURPLE)
+                                .decoration(TextDecoration.BOLD, true)
+                                .build()
+                )
+                .append(text(" to jump!"))
+                .build();
+
+        player.sendMessage(textComponent);
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage("You have left the server!");
     }
 
 }
