46ed74cbb583ca9a0f449e0be65d4d846c6202f5
Advik <advik.b@gmail.com>
2024-06-26 23:28:26 +0530

    Update PlayerDatabase.java

--- src/main/java/dev/advik/invitetree/tree/PlayerDatabase.java
+++ src/main/java/dev/advik/invitetree/tree/PlayerDatabase.java
@@ -1,23 +1,24 @@
 package dev.advik.invitetree.tree;
 
 import dev.advik.invitetree.database.DatabaseSchema;
+import java.sql.*;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PlayerDatabase implements DatabaseSchema {
     String connectionUrl;
+    Logger logger;
+    Connection dbConn;
 
-
-    @Override
-    public void setConnectionUrl(String url) {
-        connectionUrl = url;
-    }
-
-    @Override
-    public void createTables() {
-        // Create the tables
+    public PlayerDatabase(String dbURL, Logger logger) {
+        connectionUrl = dbURL;
+        this.logger = logger;
     }
 
     @Override
     public void connect() {
         // Connect to the database
+
     }
 }
