d163ed5f8d05db8568b76fbfed0954b2de848daa
Advik <advik.b@gmail.com>
2024-06-28 21:19:31 +0530

    Create LoginCommand.java

---
+++ src/main/java/dev/advik/invitetree/tree/LoginCommand.java
@@ -0,0 +1,35 @@
+package dev.advik.invitetree.tree;
+
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.entity.Player;
+import dev.advik.invitetree.impl_sqlite3.PlayerDatabase;
+import dev.advik.invitetree.storage.TRAccessToken;
+import dev.advik.invitetree.storage.TRPlayer;
+import dev.advik.invitetree.exceptions.PasswordMismatch;
+import net.kyori.adventure.text.Component;
+
+public class LoginCommand {
+
+    PlayerDatabase pDB;
+
+    LoginCommand(PlayerDatabase pDB) {
+        this.pDB = pDB;
+    }
+
+    public boolean login(Player player, String password) throws PasswordMismatch {
+        if (player == null) {
+            return false;
+        }
+        TRPlayer trPlayer = TRPlayer.fromDatabase(pDB, player.getName());
+        if (trPlayer == null) {
+            player.sendMessage("You are not registered, please register first!");
+            return;
+        }
+
+        if (trPlayer.checkPassword(password)) {
+            return true;
+        } else {
+            throw new PasswordMismatch("Password mismatch");
+        }
+    }
+}
