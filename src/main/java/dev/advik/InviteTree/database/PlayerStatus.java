502f7d4357273d78f409fa803a47c5faf629218c
Advik <advik.b@gmail.com>
2025-03-22 01:17:19 +0530

    Update PlayerStatus.java

--- src/main/java/dev/advik/InviteTree/database/PlayerStatus.java
+++ src/main/java/dev/advik/InviteTree/database/PlayerStatus.java
@@ -2,5 +2,13 @@ package dev.advik.InviteTree.database;
 
 public enum PlayerStatus {
     ALLOWED,
-    NOT_ALLOWED
+    NOT_ALLOWED;
+
+    public String getStatusString() {
+        return switch (this) {
+            case ALLOWED -> "Allowed";
+            case NOT_ALLOWED -> "Not Allowed";
+            default -> "Unknown";
+        };
+    }
 }
