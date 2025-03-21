2844181e1d56bd3e9236a37b6dfb9dfada2809f1
Advik <advik.b@gmail.com>
2025-03-21 18:18:43 +0530

    Create InviteTree.java

---
+++ src/main/java/dev/advik/inviteTree/InviteTree.java
@@ -0,0 +1,17 @@
+package dev.advik.inviteTree;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InviteTree extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
