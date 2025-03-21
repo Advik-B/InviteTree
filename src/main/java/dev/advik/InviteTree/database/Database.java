9544c939582319f7e7677007d21e8ae887c6f4b3
Advik <advik.b@gmail.com>
2025-03-21 23:38:39 +0530

    Update Database.java

--- src/main/java/dev/advik/InviteTree/database/Database.java
+++ src/main/java/dev/advik/InviteTree/database/Database.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 public class Database {
@@ -39,9 +40,16 @@ public class Database {
         } catch (Exception e) {
             logger.severe("Could not create users table: " + e.getMessage());
         }
-    }
 
-    
+        // Players table
+        // name: username | status: PlayerStatus
+        sql = "CREATE TABLE IF NOT EXISTS players (name TEXT PRIMARY KEY, status INTEGER);";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            logger.severe("Could not create players table: " + e.getMessage());
+        }
+    }
 
     public void shutdown() {
         try {
@@ -52,4 +60,58 @@ public class Database {
             logger.severe("Error closing database: " + e.getMessage());
         }
     }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    //    public String CreateInvite(String inviter, String invitee) {
+//        String sql = "INSERT INTO invites (uuid, inviter, invitee, status, invited_at, accepted_at) VALUES (?, ?, ?, ?, ?, ?);";
+//        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+//            String uuid = UUID.randomUUID().toString();
+//            stmt.setString(1, uuid);
+//            stmt.setString(2, inviter);
+//            stmt.setString(3, invitee);
+//            stmt.setInt(4, 0);
+//            stmt.setLong(5, System.currentTimeMillis());
+//            stmt.setLong(6, 0);
+//            stmt.executeUpdate();
+//            return uuid;
+//        } catch (Exception e) {
+//            logger.severe("Could not create invite: " + e.getMessage());
+//        } return null;
+//    }
+//
+//    public void AcceptInvite(String invitee, String InviteCode) {
+//        String sql = "UPDATE invites SET status = 1, accepted_at = ? WHERE invitee = ? AND uuid = ?;";
+//        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+//            stmt.setLong(1, System.currentTimeMillis());
+//            stmt.setString(2, invitee);
+//            stmt.setString(3, InviteCode);
+//            stmt.executeUpdate();
+//        } catch (Exception e) {
+//            logger.severe("Could not accept invite: " + e.getMessage());
+//        }
+//    }
+//
+//    public boolean InviteExists(String invitee) {
+//        String sql = "SELECT * FROM invites WHERE invitee = ?;";
+//        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+//            stmt.setString(1, invitee);
+//            return stmt.executeQuery().next();
+//        } catch (Exception e) {
+//            logger.severe("Could not check if invite exists: " + e.getMessage());
+//        } return false;
+//    }
+//
+//    public void RejectInvite(String invitee) {
+//        String sql = "UPDATE invites SET status = 2 WHERE invitee = ?;";
+//        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+//            stmt.setString(1, invitee);
+//            stmt.executeUpdate();
+//        } catch (Exception e) {
+//            logger.severe("Could not reject invite: " + e.getMessage());
+//        }
+//    }
+
 }
