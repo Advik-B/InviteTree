c8bf2690008c42b4f1df1c4e589b706f06906767
Advik <advik.b@gmail.com>
2025-03-21 19:38:51 +0530

    Update InviteTree.java

--- src/main/java/dev/advik/inviteTree/InviteTree.java
+++ src/main/java/dev/advik/inviteTree/InviteTree.java
@@ -2,18 +2,30 @@ package dev.advik.inviteTree;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class InviteTree extends JavaPlugin {
+
+    private Logger log;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info("InviteTree is enabled");
+        log = getLogger();
+        if (!getDataFolder().exists()) {
+            if (getDataFolder().mkdirs()) {
+                log.info("Created plugin data folder: " + getDataFolder().getAbsolutePath());
+            } else {
+                log.warning("Failed to create plugin data folder!");
+            }
+        }
+        log.info("InviteTree is enabled");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        getLogger().info("InviteTree is disabled");
+        log.info("InviteTree is disabled");
     }
 }
