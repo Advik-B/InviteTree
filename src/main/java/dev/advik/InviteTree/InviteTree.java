c72a4baf288424a7938606be99b8cab398d09ad8
Advik <advik.b@gmail.com>
2025-03-26 22:10:18 +0530

    Update InviteTree.java

--- src/main/java/dev/advik/InviteTree/InviteTree.java
+++ src/main/java/dev/advik/InviteTree/InviteTree.java
@@ -2,6 +2,7 @@ package dev.advik.InviteTree;
 
 
 import dev.advik.InviteTree.commands.InviteCommand;
+import dev.advik.InviteTree.commands.RevokeCommand;
 import dev.advik.InviteTree.commands.ViewInvitesCommand;
 import dev.advik.InviteTree.database.Database;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -26,6 +27,7 @@ public class InviteTree extends JavaPlugin {
         database = new Database(getDataFolder().getAbsolutePath(), log);
         getCommand("invite").setExecutor(new InviteCommand(this, database, log));
         getCommand("invites").setExecutor(new ViewInvitesCommand(this, database, log));
+        getCommand("inviterevoke").setExecutor(new RevokeCommand(this, database, log));
         log.info("InviteTree has been enabled!");
 
     }
